21bfd4fb2344eedda2d7bcfa2237839beae576e0
ptbud <ptbudd@gmail.com>
2018-03-02 14:41:53 -0500

    Testing Push

--- src/afrs/Application.java
+++ src/afrs/Application.java
@@ -2,6 +2,6 @@ package afrs;
 
 public class Application {
 	public static void main(String[] args) {
-		System.out.println("Application Started");
+		System.out.println("Application Started!");
 	}
 }
