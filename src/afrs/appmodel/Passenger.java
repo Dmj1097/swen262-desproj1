f8c32295153ecbdb0bc1b58147028ba6e01854e6
Dylan <dmj6144@g.rit.edu>
2018-03-04 15:38:14 -0500

    fixed if bad return statement and added documentation

--- src/afrs/appmodel/Passenger.java
+++ src/afrs/appmodel/Passenger.java
@@ -20,22 +20,39 @@ public class Passenger {
     reservations = new ArrayList<>();
   }
 
-  /*
+  /**
    * Add reservations to the passengers list of reservations
    */
   public void addReservation(Reservation r){
     reservations.add(r);
   }
 
+  /**
+   * @return name of passenger
+   */
   public String getName(){return name;}
 
+  /**
+   * checks if a given origin airport name and a given destination airport is already in this passengers reservations
+   * @param origin origin airport name
+   * @param destination destination airport name
+   * @return true if it contains it, false otherwise
+   */
   public boolean alreadyContains(String origin, String destination){
     for(Reservation res: reservations) {
-        return res.equalsTo(origin,destination);
+      if (res.equalsTo(origin,destination)) {
+        return true;
+      }
     }
     return false;
   }
 
+  /**
+   * checks if reservation is already present in this passenger's reservations, deletes if it is
+   * @param origin origin airport name
+   * @param destination destination airport name
+   * @return true if reservation was present and removed, false otherwise
+   */
   public boolean removeReservation(String origin, String destination){
     if(alreadyContains(origin, destination)){
       for(Reservation res: reservations){
@@ -48,6 +65,9 @@ public class Passenger {
     return false;
   }
 
+  /**
+   * @return all reservations tied to this passenger object
+   */
   public ArrayList<Journey> getReservations() {
     ArrayList<Journey> journies = new ArrayList<>();
     for(Reservation res: reservations){
--- src/afrs/appmodel/Reservation.java
+++ src/afrs/appmodel/Reservation.java
@@ -18,19 +18,33 @@ public class Reservation {
     this.journey = journey;
   }
 
-
+  /**
+   * compares two reservations to see if they match
+   * @param origin origin airport name being used for compare
+   * @param destination destination aiport name being used for compare
+   * @return true if equal, false otherwise
+   */
   public boolean equalsTo(String origin, String destination){
       return (journey.getOrigin().equals(origin) && journey.getDestination().equals(destination));
   }
 
+  /**
+   * @return origin airport name from journey
+   */
   public String getOrigin(){
     return journey.getOrigin();
   }
 
+  /**
+   * @return destination airport name from journey
+   */
   public String getDestination(){
     return journey.getDestination();
   }
 
+  /**
+   * @return gets journey object tied to reservation
+   */
   public Journey getJourney(){
     return journey;
   }
